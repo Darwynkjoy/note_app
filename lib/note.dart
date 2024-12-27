139d7b1a20002a5ce4f62167d844bd5137c79045
darwynkjoy <darwynkjoy@gmail.com>
2024-12-27 11:01:51 +0530

    daily push

--- lib/note.dart
+++ lib/note.dart
@@ -19,7 +19,22 @@ class _notepageState extends State<Notepage>{
 
   void initState(){
     super.initState();
-    box=Hive.box("mybox");
+    openbox();
+  }
+
+  openbox()async{
+    box=await Hive.openBox("mybox");
+    loadTodoItems();
+  }
+
+    void loadTodoItems(){
+    List<Map<String,String>>? task=box.get("notes")?.cast<String>();
+    print("tasks loaded:$task");
+    if(task !=null){
+      setState(() {
+        notes=task;
+      });
+    }
   }
 
 
@@ -31,10 +46,36 @@ class _notepageState extends State<Notepage>{
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
-      body: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: notes.length), itemBuilder: (context,index){
-        return GridTile(
-          child: Column());
-      }),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,childAspectRatio: 0.8,crossAxisSpacing: 10,mainAxisSpacing: 10),itemCount: notes.length, itemBuilder: (context,index){
+          return GridTile(
+            child: Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),color: const Color.fromARGB(155, 255, 246, 164),),
+              child:  Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(child: Text("${notes[index]["title"]}",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.amber),)),
+                Text("${notes[index]["category"]}",style: TextStyle(fontSize: 18,color: Colors.grey),),
+                Text("${notes[index]["description"]}",style: TextStyle(fontSize: 15,color: Colors.grey),),
+                
+                SizedBox(height: 100,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("${notes[index]["date"]}",style: TextStyle(fontSize: 17,color: Colors.black),),
+                    IconButton(onPressed: (){
+                      setState(() {
+                        notes.removeAt(index);
+                      });
+                    }, icon: Icon(Icons.delete),iconSize: 30,color: Colors.amber,)
+                  ],
+                )
+                ],
+            )));
+        }),
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.amber,
         onPressed: (){
@@ -66,7 +107,7 @@ class _notepageState extends State<Notepage>{
                     ElevatedButton(
                       style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),
-                        backgroundColor: Colors.amber
+                        backgroundColor: Colors.amber,
                       ),
                       onPressed: (){
                         setState(() {
@@ -93,7 +134,9 @@ class _notepageState extends State<Notepage>{
                         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),
                         backgroundColor: Colors.amber
                       ),
-                      onPressed: (){}, child: Text("Cancel",style: TextStyle(fontSize: 20,color: Colors.white),)),
+                      onPressed: (){
+                        Navigator.pop(context);
+                      }, child: Text("Cancel",style: TextStyle(fontSize: 20,color: Colors.white),)),
                   ],
                 )
               ],
