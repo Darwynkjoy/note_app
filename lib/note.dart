a3f6fcedb51ac68180f921a8657eb63cabe92891
darwynkjoy <darwynkjoy@gmail.com>
2024-12-27 13:09:01 +0530

    daily push

--- lib/note.dart
+++ lib/note.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 
@@ -30,13 +32,82 @@ class _notepageState extends State<Notepage>{
     void loadTodoItems(){
     List<Map<String,String>>? task=box.get("notes")?.cast<String>();
     print("tasks loaded:$task");
-    if(task !=null){
-      setState(() {
-        notes=task;
-      });
+    setState(() {
+      notes=task!;
+    });
     }
-  }
 
+    void edit(int index){
+      titleContoller.text=notes[index]["title"]??"";
+      categoryContoller.text=notes[index]["category"]??"";
+      descriptionContoller.text=notes[index]["description"]??"";
+      dateContoller.text=notes[index]["date"]??"";
+
+      showModalBottomSheet(context: context, builder: (BuildContext context){
+          return Container(
+            padding: EdgeInsets.only(left: 15,right: 15,top: 15,bottom: 15),
+            height: 400,
+            width: 500,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color: const Color.fromARGB(255, 255, 255, 255)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextField(
+                  controller: titleContoller,
+                  decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),labelText: "Title",labelStyle: TextStyle(color: Colors.amber,fontSize: 20)),),
+                TextField(
+                  controller: categoryContoller,
+                  decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),labelText: "Category",labelStyle: TextStyle(color: Colors.amber,fontSize: 20)),),
+                TextField(
+                  controller: descriptionContoller,
+                  decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),labelText: "Description",labelStyle: TextStyle(color: Colors.amber,fontSize: 20)),),
+                TextField(
+                  controller: dateContoller,
+                  decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),labelText: "Date",labelStyle: TextStyle(color: Colors.amber,fontSize: 20)),),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),
+                        backgroundColor: Colors.amber,
+                      ),
+                      onPressed: (){
+                        setState(() {
+                          notes[index]=({
+                            "title":titleContoller.text,
+                            "category":categoryContoller.text,
+                            "description":descriptionContoller.text,
+                            "date":dateContoller.text
+                            });
+                            box.put("notes", notes.map((e)=>Map<String,dynamic>.from(e)).toList());
+                        });
+                        titleContoller.clear();
+                        categoryContoller.clear();
+                        descriptionContoller.clear();
+                        dateContoller.clear();
+
+                        Navigator.pop(context);
+                      }, child: Text("Save",style: TextStyle(fontSize: 20,color: Colors.white),)),
+
+                    SizedBox(width: 15,),
+
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10),),
+                        backgroundColor: Colors.amber
+                      ),
+                      onPressed: (){
+                        Navigator.pop(context);
+                      }, child: Text("Cancel",style: TextStyle(fontSize: 20,color: Colors.white),)),
+                  ],
+                )
+              ],
+            ),
+          );
+        });
+    }
 
   @override
   Widget build(BuildContext context){
@@ -60,13 +131,15 @@ class _notepageState extends State<Notepage>{
                   children: [
                     Center(child: Text("${notes[index]["title"]}",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.amber),)),
                     SizedBox(width: 20,),
-                    IconButton(onPressed: (){}, icon: Icon(Icons.edit,color: Colors.amber,))
+                    IconButton(onPressed: (){
+                      edit(index);
+                    }, icon: Icon(Icons.edit,color: Colors.amber,))
                   ],
                 ),
                 Text("${notes[index]["category"]}",style: TextStyle(fontSize: 18,color: Colors.grey),),
                 Text("${notes[index]["description"]}",style: TextStyle(fontSize: 15,color: Colors.grey),),
                 
-                SizedBox(height: 90,),
+                SizedBox(height: 80,),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -74,6 +147,7 @@ class _notepageState extends State<Notepage>{
                     IconButton(onPressed: (){
                       setState(() {
                         notes.removeAt(index);
+                        box.put("notes", notes.map((e)=>Map<String,dynamic>.from(e)).toList());
                       });
                     }, icon: Icon(Icons.delete),iconSize: 30,color: Colors.amber,)
                   ],
